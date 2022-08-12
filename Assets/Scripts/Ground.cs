e09f3e550a0acc0c47756e38e1165fb698daae65
AlexHailstone <alexander.ramsbey@gmail.com>
2022-08-12 11:32:57 -0400

    Fixed the Shop issues
    You can now select the items in the shop freely and place them into the world without overlapping. This also allows the player to utilize currency.

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -5,37 +5,79 @@ using UnityEngine.EventSystems;
 
 public class Ground : MonoBehaviour
 {
-
+    BuildManager buildManager;
     //I need this turret to be dynamically selected from the shop GetSelectedTurrent()
+    public Vector3 positionOffset;
+
+
+    [HideInInspector]
     public GameObject turret;
+    [HideInInspector]
+    public TurretBlueprint turretBlueprint;
+    [HideInInspector]
+    public bool isUpgraded = false;
 
 
-    void Update()
+
+    private void Start()
+	{
+        buildManager = BuildManager.instance;
+
+    }
+
+void Update()
 	{
         if (Input.GetMouseButtonDown(0))
 		{
             if (EventSystem.current.IsPointerOverGameObject())
                 return;
 
-            PlaceOnGround();
+            BuildTurret(buildManager.GetTurretToBuild());
 		}
 	}
 
-
+    
     void PlaceOnGround()
     {
        
-        Collider col = GetComponent<Collider>();
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
+        
 
-        if (Physics.Raycast(ray, out hit, 1000))
+    }
+
+
+    void BuildTurret(TurretBlueprint blueprint)
+    {
+        if (PlayerStats.Money >= blueprint.cost)
         {
-            //if this runs into only terrain collider, no other turret can build.
-            if (hit.collider.gameObject.name == "Ground")
+
+            PlayerStats.Money -= blueprint.cost;
+
+            Collider col = GetComponent<Collider>();
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 1000))
             {
-                GameObject _turretToBuild = (GameObject)Instantiate(turret, hit.point, Quaternion.identity);
+                //if this runs into only terrain collider, no other turret can build.
+                if (hit.collider.gameObject.name == "Ground")
+                {
+                    //GameObject _turretToBuild = (GameObject)Instantiate(turret, hit.point, Quaternion.identity);
+
+                    //build a turret
+                    GameObject _turret = (GameObject)Instantiate(blueprint.prefab, hit.point, Quaternion.identity);
+                    turret = _turret;
+                    turretBlueprint = blueprint;
+
+                    //BuildEffect
+                    Debug.Log("Turret Built!");
+                    GameObject effect = (GameObject)Instantiate(buildManager.buildEffect, hit.point, Quaternion.identity);
+                    Destroy(effect, 5f);
+                }
             }
+        }
+        else
+        {
+            Debug.Log("Not enough currency");
         }
 
     }
